0ca56cefb8a4ce3a97e53dda9913cd33edc4bcef
Alejandro Abad <aabadg1@alumnos.nebrija.es>
2021-10-28 14:44:47 +0200

    Corregido un pequeño error

--- ejercicio_entregar_7.py
+++ ejercicio_entregar_7.py
@@ -3,14 +3,15 @@ while True:
     nombre= input('Nombre:')
     if nombre == 'fin':
         break
-    apellido1 = input('primer apellido:')
-    apellido2= input('segundo apellido:')
-    telefon= input('telefono movil:')
+    apellido1 = input('Primer apellido:')
+    apellido2= input('Segundo apellido:')
+    telefono= input('Telefono movil:')
     #CREAMOS UN DICCIONARIO
     linea= {}
     linea['Nombre'] = nombre
     linea['1er apellido'] = apellido1
     linea['2do apellido'] = apellido2
+    linea['Teléfono'] = telefono
     #AÑADIMOS DICCIONARIO A LA LISTA
     datos.append(linea)
 
